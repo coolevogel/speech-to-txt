84843eaed223f60be222e74740e48255681dc135
coolevogel <90614626+coolevogel@users.noreply.github.com>
2022-05-30 10:05:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const message = document.getElementById("message");
+const output = document.getElementById("result");
+const image1 = document.getElementById("image1");
+
+startRecognition = () => {
+  if (SpeechRecognition !== undefined) { // test if speechrecognitio is supported
+    let recognition = new SpeechRecognition();
+    recognition.lang = 'nl-NL';//en-US'; // which language is used?
+    recognition.interimResults = false; // https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition/interimResults
+    recognition.continuous = false; // https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition/continuous
+   
+    recognition.onstart = () => {
+      message.innerHTML = `Starting listening, speak in the microphone please<br>Say "help me" for help`;
+      output.classList.add("hide"); // hide the output
+    };
+
+    recognition.onspeechend = () => {
+      message.innerHTML = `I stopped listening `;
+      recognition.stop();
+    };
+
+    recognition.onresult = (result) => {
+      let transcript = result.results[0][0].transcript;
+      let confidenceTranscript= Math.floor(result.results[0][0].confidence * 100); // calc. 'confidence'
+      output.classList.remove("hide"); // show the output
+      output.innerHTML = `I'm ${confidenceTranscript}% certain you just said: <b>${transcript}</b>`;
+      actionSpeech(transcript);
+    };
+
+    recognition.start();
+  } 
+  else {  // speechrecognition is not supported
+    message.innerHTML = "sorry speech to text is not supported in this browser";
+  }
+};
+
+// process speech results
+actionSpeech = (speechText) => {
+  speechText = speechText.toLowerCase().trim(); // trim spaces + to lower case
+  console.log(speechText); // debug 
+  switch(speechText){ 
+    // switch evaluates using stric comparison, ===
+    case "zwart":
+      document.body.style.background = "#000000";
+      document.body.style.color="#FFFFFF";
+      break;
+    case  "reset":
+      document.body.style.background = "#ffe6ab";
+      document.body.style.color="#000000";
+      image1.classList.add("hide"); // hide image (if any)
+      break;
+    case "foto": // let op, "fall-through"
+      image1.src = "./img/matthijs.jpg";
+      image1.style.width = "400px";
+      image1.classList.remove("hide") // show image
+      break;
+    case "volgende pagina":
+      window.open("https://www.ma-web.nl/", "_self");
+      break;
+    case "help mij":
+      alert("Valid speech commands: black,  reset, next page");
+      break;
+      case "github":
+        window.open("https://github.com/coolevogel", "_self");
+        break;
+        case "stoel":
+          window.open("https://www.ikea.com/nl/nl/p/ekenaset-fauteuil-hillared-antraciet-10429275/", "_self");
+          break;
+    default:
+      // do nothing yet
+  }
+}
